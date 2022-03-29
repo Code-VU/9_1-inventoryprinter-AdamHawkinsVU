4646213c9dc1a7b38db1916dc89acd5e6c0670a4
Adam Hawkins <Adam.Hawkins@vu.com>
2022-03-29 12:07:33 -0500

    first attempt

--- displayInventory.py
+++ displayInventory.py
@@ -1,6 +1,13 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    # your code goes here
 
-##displayInventory(stuff)
+    items_ammount = 0
+    
+    print ("Inventory:")
+    for item in sorted(inventory):
+        print(str(inventory[item]) + ' ' + item)
+        items_ammount += inventory[item]
+    print("Total number of items: " +str(items_ammount))
+
+displayInventory(stuff)
